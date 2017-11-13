46b5f259a5cd6d68612e7333fa370b5bbb26a5d5
Allan <Allan@Fenris>
2017-11-13 18:53:54 -0500

    Initial Commit

---
+++ src/binarySearchTree/BinarySearchTreeA1.java
@@ -0,0 +1,25 @@
+package binarySearchTree;
+
+public class BinarySearchTreeA1<T> extends BinarySearchTreeAbstract<T>{
+	/*
+	 * Removes and returns an element in a BST, removing 
+	 */
+	public T getElt(T x) {
+		//TODO
+		return null;
+	}
+	/*
+	 * Inserts an element into this according to standard BST convention with duplicate entries
+	 * going to the right
+	 * @Param X
+	 * The element to be inserted
+	 */
+	public T insert(T x) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	public void setNewRoot(T newRoot) {
+		//TODO
+	}
+
+}
